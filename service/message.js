eb2addca7f6d36d4fe6fc16cc29af2ca58a6a1d3
linghan <815495626@qq.com>
2019-04-27 18:44:51 +0800

    ADD: some api

---
+++ service/message.js
@@ -0,0 +1,21 @@
+import Request from '../utils/request'
+
+export default class Message {
+    constructor() {
+        this._baseUrl = 'https://www.weplann.cn/message/';
+        this._request = new Request();
+        this._request.setErrorHandler(this.errorHander);
+    }
+
+    errorHander(err) {
+        console.log(err);
+    }
+
+    create(data) {
+        return this._request.postRequest(this._baseUrl, data).then(res => res.data);
+    }
+
+    get(activityId) {
+        return this._request.getRequest(this._baseUrl + '?activityId=', activityId);
+    }
+}
---
+++ service/star.js
@@ -0,0 +1,21 @@
+import Request from '../utils/request'
+
+export default class Star {
+    constructor() {
+        this._baseUrl = 'https://www.weplann.cn/star/';
+        this._request = new Request();
+        this._request.setErrorHandler(this.errorHander);
+    }
+
+    errorHander(err) {
+        console.log(err);
+    }
+
+    create(data) {
+        return this._request.postRequest(this._baseUrl, data).then(res => res.data);
+    }
+
+    delete(data) {
+        return this._request.deleteRequest(this._baseUrl, data).then(res => res.data);
+    }
+}
